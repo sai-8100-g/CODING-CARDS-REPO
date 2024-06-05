23caa96c16b21452d8f4785d00366dfbf31eaf16
sai kumar <saikumarb8100@gmail.com>
2024-06-05 10:53:02 +0000

    coding practice with cards in IDE

---
+++ index.js
@@ -0,0 +1,21 @@
+const element = (
+  // Write your code here.
+
+  <div className='main-container'>
+    <div className='content-container'>
+      <h1>Congratulations</h1>
+      <div className='profile-container'>
+        <div>
+          <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png' />
+          <p className="name">Sai Kumar</p>
+        </div>
+        <p>Congratulations you to Sucessfully completing the bootcamp</p>
+        <div>
+          <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png' />
+        </div>
+      </div>
+    </div>
+  </div>
+)
+
+ReactDOM.render(element, document.getElementById('root'))
